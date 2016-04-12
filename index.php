6fcf7ae9e206457a4a0122fcd7db86f26d13da73
samora <samargaber3@gmail.com>
2016-04-12 15:39:04 +0200

    index task

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form action="insert.php" method="post">
+name:<input name="s_name" type="text" /><br/>
+gender:
+male:<input name="s_gender" type="radio" value="male" />
+female:<input name="s_gender" type="radio" value="female" /><br/>
+password:<input name="s_password" type="password" /><br/>
+<a href="insert.php"><input name="go" type="submit" value="go" /></a>
+</form>
+</body>
+</html>
